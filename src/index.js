e6d8ab17bfe9b981253193cf6b18b92a4674eb7c
Ali000 <ali.al-banna@outlook.com>
2022-03-14 11:49:13 +0300

    clickHandler added

--- src/index.js
+++ src/index.js
@@ -1,40 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css";
 // function Greeting() {
 //   return <h4>hello world</h4>;
 // }
 
+const books = [
+  {
+    author: "Adam Wallace",
+    type: "hardcover",
+    price: "7.43",
+    title: "How to Catch a Leprechaun",
+    cover:
+      "https://images-na.ssl-images-amazon.com/images/I/81hSK0bkkFL._AC_UL604_SR604,400_.jpg",
+  },
+
+  {
+    author: "Colleen Hoover",
+    type: "Paperback",
+    price: "45.52",
+    title: "It Ends with Us",
+    cover:
+      "https://images-na.ssl-images-amazon.com/images/I/51irsfz72HL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  },
+];
+
 function Booklist() {
+  const clickHandler = (title) => console.log(title);
+  return (
+    <article>
+      {books.map((book, index) => {
+        return (
+          <>
+            <Book key={index} book={book} />
+            <button onClick={() => clickHandler(book.title)}>View</button>
+          </>
+        );
+      })}
+      {/* <AnotherBook book={secondBook} /> */}
+    </article>
+  );
+}
+
+function Book({ book }) {
+  // const { cover, title, author, type, price } = book;
   return (
     <>
-      <Book
-        author="Colleen Hoover"
-        type="Paperback"
-        price="45.52"
-        title="It Ends with Us"
-        cover="https://images-na.ssl-images-amazon.com/images/I/51irsfz72HL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-      />
-      <Book
-        author="Colleen Hoover"
-        type="Paperback"
-        price="45.52"
-        title="It Ends with Us"
-        cover="https://images-na.ssl-images-amazon.com/images/I/51irsfz72HL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
-      />
+      <Image book_img={book.cover} />
+      <Title title={book.title} />
+      <Author author={book.author} />
+      <Type type={book.type} />
+      <Price price={book.price} />
     </>
   );
 }
 
-function Book({ cover, title, author, type, price }) {
-  // const { cover, title, author, type, price } = book;
+function AnotherBook({ book }) {
   return (
     <>
-      <Image book_img={cover} />
-      <Title title={title} />
-      <Author author={author} />
-      <Type type={type} />
-      <Price price={price} />
+      <img src={book.cover} alt="book" />;<p>{book.title}</p>;
+      <p>{book.author} </p>;<h6>{book.type}</h6>;<h6>{book.price}</h6>;
     </>
   );
 }
@@ -48,7 +74,7 @@ const Type = ({ type }) => <h6>{type}</h6>;
 const Title = ({ title }) => <p>{title}</p>;
 
 const Price = ({ price }) => {
-  return <h5>{price}</h5>;
+  return <h6>{price}</h6>;
 };
 
 const Image = ({ book_img }) => {
